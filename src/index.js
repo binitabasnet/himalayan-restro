d430822fbacfc45b2a0d8d3c881b433681b263a6
binita basnet <binita.basnet@aptcluster.com>
2022-12-13 17:28:20 +0545

    update:added redux in cart

--- src/index.js
+++ src/index.js
@@ -5,7 +5,25 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./Redux/store";
+// import { store } from "./Redux/store";
+import { configureStore } from "@reduxjs/toolkit";
+
+import productsReducer, { productsFetch } from "./slices/productsSlice";
+import cartReducer, { getTotals } from "./slices/cartSlice";
+import { productsApi } from "./slices/productsApi";
+
+const store = configureStore({
+  reducer: {
+    products: productsReducer,
+    cart: cartReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
+});
+
+store.dispatch(productsFetch());
+store.dispatch(getTotals());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
